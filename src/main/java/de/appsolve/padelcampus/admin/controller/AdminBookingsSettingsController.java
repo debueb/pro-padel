ae1de6cd2508173ed1fd23785a1c4f958f2e8218
dwissk <dominik.wisskirchen@ebf.de>
2015-01-25 10:02:40 +0100

    organize imports

--- src/main/java/de/appsolve/padelcampus/admin/controller/AdminBookingsSettingsController.java
+++ src/main/java/de/appsolve/padelcampus/admin/controller/AdminBookingsSettingsController.java
@@ -24,6 +24,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
