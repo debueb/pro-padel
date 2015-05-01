fc4019dfcd8899eb8819b2828705afdc9ad94e3a
dwissk <dominik.wisskirchen@ebf.de>
2015-05-01 11:38:17 +0200

    enable PUT for multipart form data

---
+++ src/main/java/de/appsolve/padelcampus/resolver/PutAwareCommonsMultipartResolver.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.appsolve.padelcampus.resolver;
+
+import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+/**
+ *
+ * @author dominik
+ */
+public class PutAwareCommonsMultipartResolver extends CommonsMultipartResolver {
+
+    private static final String MULTIPART = "multipart/";
+
+    @Override
+    public boolean isMultipart(HttpServletRequest request) {
+        return request != null && isMultipartContent(request);
+    }
+
+    /**
+     * Utility method that determines whether the request contains multipart
+     * content.
+     * 
+     * @param request The servlet request to be evaluated. Must be non-null.
+     * 
+     * @return <code>true</code> if the request is multipart; {@code false}
+     * otherwise.
+     * 
+     * @see ServletFileUpload#isMultipartContent(HttpServletRequest)
+     */
+    public static final boolean isMultipartContent(HttpServletRequest request) {
+        String contentType = request.getContentType();
+        if (contentType == null) {
+            return false;
+        }
+        return contentType.toLowerCase(Locale.ENGLISH).startsWith(MULTIPART);
+    }
+
+}
