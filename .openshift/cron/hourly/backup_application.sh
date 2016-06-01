daaeac779b2c01c556f25165c6110393227106e5
dwissk <dominik.wisskirchen@ebf.de>
2016-06-01 15:29:33 +0200

    copy data dir to tmp dir

--- .openshift/cron/hourly/backup_application.sh
+++ .openshift/cron/hourly/backup_application.sh
@@ -12,9 +12,12 @@ ${OPENSHIFT_REPO_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_REPO_DIR}/dropbox_uploa
 ./dropbox_uploader.sh upload ${OPENSHIFT_TMP_DIR}/backup.sql.gz ${TODAY}/${OPENSHIFT_APP_NAME}_mysql_${TODAY}.sql.gz
 rm ${OPENSHIFT_TMP_DIR}/backup.sql.gz
 
-tar -zcvf ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz ${OPENSHIFT_DATA_DIR}/
+mkdir ${OPENSHIFT_TMP_DIR}/data
+cp -R ${OPENSHIFT_DATA_DIR} ${OPENSHIFT_TMP_DIR}/data
+tar -zcf ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz ${OPENSHIFT_TMP_DIR}/data
 
 ${OPENSHIFT_REPO_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_REPO_DIR}/dropbox_uploader.config upload ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz ${TODAY}/${OPENSHIFT_APP_NAME}_data_${TODAY}.tar.gz
 rm ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz
+rm -rf ${OPENSHIFT_TMP_DIR}/data
 
 #fi
