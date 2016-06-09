1e79c664fd500f9d2fe4439a64913844f45eabf6
Dominik <d.wisskirchen@gmail.com>
2016-06-09 14:06:14 +0200

    move dropbox uploader

--- .openshift/cron/hourly/backup_application.sh
+++ .openshift/cron/hourly/backup_application.sh
@@ -8,15 +8,15 @@ TODAY=$(date +"%Y_%m_%d");
 mysqldump -h $OPENSHIFT_MYSQL_DB_HOST -P $OPENSHIFT_MYSQL_DB_PORT -u $OPENSHIFT_MYSQL_DB_USERNAME --password=$OPENSHIFT_MYSQL_DB_PASSWORD $OPENSHIFT_APP_NAME > ${OPENSHIFT_TMP_DIR}/backup.sql
 gzip -f ${OPENSHIFT_TMP_DIR}/backup.sql
 
-${OPENSHIFT_REPO_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_REPO_DIR}/dropbox_uploader.config mkdir ${TODAY}
-${OPENSHIFT_REPO_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_REPO_DIR}/dropbox_uploader.config upload ${OPENSHIFT_TMP_DIR}/backup.sql.gz ${TODAY}/${OPENSHIFT_APP_NAME}_mysql_${TODAY}.sql.gz
+${OPENSHIFT_DATA_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_DATA_DIR}/dropbox_uploader.config mkdir ${TODAY}
+${OPENSHIFT_DATA_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_DATA_DIR}/dropbox_uploader.config upload ${OPENSHIFT_TMP_DIR}/backup.sql.gz ${TODAY}/${OPENSHIFT_APP_NAME}_mysql_${TODAY}.sql.gz
 rm ${OPENSHIFT_TMP_DIR}/backup.sql.gz
 
 mkdir ${OPENSHIFT_TMP_DIR}/data
 cp -R ${OPENSHIFT_DATA_DIR} ${OPENSHIFT_TMP_DIR}/data
 tar -zcf ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz ${OPENSHIFT_TMP_DIR}/data
 
-${OPENSHIFT_REPO_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_REPO_DIR}/dropbox_uploader.config upload ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz ${TODAY}/${OPENSHIFT_APP_NAME}_data_${TODAY}.tar.gz
+${OPENSHIFT_DATA_DIR}/dropbox_uploader.sh -f ${OPENSHIFT_DATA_DIR}/dropbox_uploader.config upload ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz ${TODAY}/${OPENSHIFT_APP_NAME}_data_${TODAY}.tar.gz
 rm ${OPENSHIFT_TMP_DIR}/data_${TODAY}.tar.gz
 rm -rf ${OPENSHIFT_TMP_DIR}/data
 
