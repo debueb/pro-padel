fe6c7314a1183c737d8e734bc0037b5c71e96765
Dominik <d.wisskirchen@gmail.com>
2016-09-26 17:14:34 +0200

    change disk usage level

--- .openshift/cron/daily/check_space.sh
+++ .openshift/cron/daily/check_space.sh
@@ -3,8 +3,8 @@
 cd
 DISK_USAGE=`du -s 2>/dev/null |awk '{print $1}'`
 
-if [ ${DISK_USAGE} -gt 800000 ]
-then mail -s "Disk quota has reached 80%" d.wisskirchen@gmail.com <<EOF
+if [ ${DISK_USAGE} -gt 900000 ]
+then mail -s "Disk quota has reached 90%" d.wisskirchen@gmail.com <<EOF
 Disk quota has reached ${DISK_USAGE}
 EOF
 fi
