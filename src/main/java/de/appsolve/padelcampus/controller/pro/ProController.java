6b8c19967838a86d4c5f770819cf8937addbcb54
Dominik <d.wisskirchen@gmail.com>
2017-01-05 16:42:55 +0100

    redirect subdomains

--- src/main/java/de/appsolve/padelcampus/controller/pro/ProController.java
+++ src/main/java/de/appsolve/padelcampus/controller/pro/ProController.java
@@ -5,9 +5,13 @@
  */
 package de.appsolve.padelcampus.controller.pro;
 
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  *
@@ -18,7 +22,21 @@ import org.springframework.web.servlet.ModelAndView;
 public class ProController {
     
     @RequestMapping
-    public ModelAndView index(){
+    public ModelAndView index(HttpServletRequest request){
+        String serverName = request.getServerName();
+        if (!StringUtils.isEmpty(serverName)){
+            String[] domainParts = serverName.split("\\.");
+            if (domainParts.length>2){
+                String url = request.getScheme()+"://"+domainParts[domainParts.length-2]+"."+domainParts[domainParts.length-1];
+                if (request.getScheme().equals("http") && request.getServerPort() != 80){
+                    url += ":"+request.getServerPort();
+                }
+                url += request.getRequestURI();
+                RedirectView rv = new RedirectView(url);
+                rv.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+                return new ModelAndView(rv);
+            }
+        }
         return new ModelAndView("pro/index");
     }
 }
