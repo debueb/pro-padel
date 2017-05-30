9e2842de9535614c9bbdd8e79cf11ef88b1a27fd
Dominik <d.wisskirchen@gmail.com>
2017-05-30 20:20:24 +0200

    add Cache-Control header

--- src/main/java/de/appsolve/padelcampus/admin/controller/CssController.java
+++ src/main/java/de/appsolve/padelcampus/admin/controller/CssController.java
@@ -8,8 +8,12 @@ package de.appsolve.padelcampus.admin.controller;
 import de.appsolve.padelcampus.controller.BaseController;
 import de.appsolve.padelcampus.utils.HtmlResourceUtil;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,13 +32,19 @@ public class CssController extends BaseController{
     
     @ResponseBody
     @RequestMapping(value="all.min.stylesheet", produces = "text/css")
-    public String getAllMinCss(HttpServletRequest request) throws IOException{
+    public String getAllMinCss(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        addCacheControlResponseHeader(response);
         return htmlResourceUtil.getAllMinCss(request.getServletContext(), "");
     }
     
     @ResponseBody
     @RequestMapping(value="{customerName}/all.min.stylesheet", produces = "text/css")
-    public String getAllMinCss(HttpServletRequest request, @PathVariable("customerName") String customerName) throws IOException{
+    public String getAllMinCss(HttpServletRequest request, HttpServletResponse response, @PathVariable("customerName") String customerName) throws IOException{
+        addCacheControlResponseHeader(response);
         return htmlResourceUtil.getAllMinCss(request.getServletContext(), customerName);
+    }
+
+    private void addCacheControlResponseHeader(HttpServletResponse response) {
+        response.addHeader(HttpHeaders.CACHE_CONTROL, CacheControl.maxAge(4, TimeUnit.HOURS).getHeaderValue());
     }
 }
