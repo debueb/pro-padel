a67e63a2da57e35bd98eceec638ac3ccddebc54a
Dominik <d.wisskirchen@gmail.com>
2017-08-27 13:11:52 +0200

    do not require data directory for less compilation

--- src/main/java/de/appsolve/padelcampus/utils/HtmlResourceUtil.java
+++ src/main/java/de/appsolve/padelcampus/utils/HtmlResourceUtil.java
@@ -40,44 +40,40 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 /**
- *
  * @author dominik
  */
 @Component
 public class HtmlResourceUtil {
-    
+
     private static final Logger LOG = Logger.getLogger(HtmlResourceUtil.class);
-    
+
     private LessCompiler lessCompiler;
-    
+
     @Autowired
     CustomerDAOI customerDAO;
-    
+
     @Autowired
     CssAttributeDAOI cssAttributeDAO;
-    
+
     @Autowired
     CssAttributeBaseDAOI cssAttributeBaseDAO;
-    
-    @Autowired
-    CustomerUtil customerUtil;
-    
-    private static final String VARIABLES_LESS  = "/static/less/variables.less";
-    private static final String PROJECT_LESS    = "/static/less/90_project.less";
-    private static final String PROJECT_CSS     = "/static/css/90_project.css";
-    private static final String LOADER_LESS     = "/static/less/96_loader.less";
-    private static final String LOADER_CSS      = "/static/css/96_loader.css";
-    private static final String BOOTSTRAP_LESS  = "/static/less/10_bootstrap.less"; 
-    private static final String BOOTSTRAP_CSS   = "/static/css/10_bootstrap.css";
 
-    private static final String FOLDER_CSS      = "/static/css";
-    private static final String FOLDER_LESS     = "/static/less";
-    
+    private static final String VARIABLES_LESS = "/static/less/variables.less";
+    private static final String PROJECT_LESS = "/static/less/90_project.less";
+    private static final String PROJECT_CSS = "/static/css/90_project.css";
+    private static final String LOADER_LESS = "/static/less/96_loader.less";
+    private static final String LOADER_CSS = "/static/css/96_loader.css";
+    private static final String BOOTSTRAP_LESS = "/static/less/10_bootstrap.less";
+    private static final String BOOTSTRAP_CSS = "/static/css/10_bootstrap.css";
+
+    private static final String FOLDER_CSS = "/static/css";
+    private static final String FOLDER_LESS = "/static/less";
+
     private static final String ALL_MIN_CSS = "/static/css/all.min.css";
 
     public void updateCss(final ServletContext context) throws Exception {
         List<Customer> customers = customerDAO.findAll();
-        if (customers.isEmpty()){
+        if (customers.isEmpty()) {
             applyCustomerCss(context, getDefaultCssAttributes(), "");
         } else {
             lessCompiler = new LessCompiler();
@@ -87,7 +83,7 @@ public class HtmlResourceUtil {
             ExecutorService executor = Executors.newFixedThreadPool(availableProcessors);
             List<FutureTask<Void>> taskList = new ArrayList<>();
 
-            for (final Customer customer: customers){
+            for (final Customer customer : customers) {
                 FutureTask<Void> futureTask = new FutureTask<>(new Callable<Void>() {
                     @Override
                     public Void call() throws Exception {
@@ -101,97 +97,77 @@ public class HtmlResourceUtil {
                 });
                 taskList.add(futureTask);
                 executor.execute(futureTask);
-            } 
-            for (FutureTask task: taskList) {
+            }
+            for (FutureTask task : taskList) {
                 task.get();
             }
             executor.shutdown();
         }
     }
-    
-    public void updateCss(ServletContext context, CustomerI customer) throws Exception{
+
+    public void updateCss(ServletContext context, CustomerI customer) throws Exception {
         Map<String, Object> attributeMap = new HashMap<>();
         attributeMap.put("customer", customer);
         List<CssAttribute> cssAttributes = cssAttributeBaseDAO.findByAttributes(attributeMap);
-        if (cssAttributes.isEmpty()){
+        if (cssAttributes.isEmpty()) {
             cssAttributes = getDefaultCssAttributes();
         }
         applyCustomerCss(context, cssAttributes, customer.getName());
     }
-    
+
     private void applyCustomerCss(ServletContext context, List<CssAttribute> cssAttributes, String customerName) throws Exception {
         if (!cssAttributes.isEmpty()) {
-            //PROBLEM: on openshift, the war file is not extracted. Thus, sortedFiles cannot be overwritten and must be read with context.getResource...
-
-            File destDir = customerUtil.getCustomerDir(customerName);
+            Path tempPath = Files.createTempDirectory(customerName);
+            File tempDir = tempPath.toFile();
 
             //copy css sortedFiles to data directory
-            copyResources(context, FOLDER_CSS, destDir);
-            //FileUtils.copyDirectory(new File(rootPath + FOLDER_CSS), new File(DATA_DIR + FOLDER_CSS));
-            //remove all.min.css from data directory as we do not want to concatenate it with itself
-            File allMinCssFile = new File(destDir, ALL_MIN_CSS);
-            boolean delete = allMinCssFile.delete();
-            if (!delete){
-                LOG.warn("Unable to delete "+allMinCssFile);
-            }
+            copyResources(context, FOLDER_CSS, tempDir);
 
             //copy less sortedFiles
-            copyResources(context, FOLDER_LESS, destDir);
+            copyResources(context, FOLDER_LESS, tempDir);
 
             //replace variables in variables.less, 90_project.less, 96_loader.less
-            replaceVariables(context, cssAttributes, VARIABLES_LESS, destDir);
-            replaceVariables(context, cssAttributes, PROJECT_LESS, destDir);
-            replaceVariables(context, cssAttributes, LOADER_LESS, destDir);
+            replaceVariables(context, cssAttributes, VARIABLES_LESS, tempDir);
+            replaceVariables(context, cssAttributes, PROJECT_LESS, tempDir);
+            replaceVariables(context, cssAttributes, LOADER_LESS, tempDir);
 
 
             //compile less and overwrite css sortedFiles in data directory
-            if (lessCompiler == null){
+            if (lessCompiler == null) {
                 lessCompiler = new LessCompiler();
             }
-            lessCompiler.compile(new File(destDir, PROJECT_LESS), new File(destDir, PROJECT_CSS));
-            lessCompiler.compile(new File(destDir, BOOTSTRAP_LESS), new File(destDir, BOOTSTRAP_CSS));
-            lessCompiler.compile(new File(destDir, LOADER_LESS), new File(destDir, LOADER_CSS));
-            
-            Path allMinCssPath = new File(destDir, ALL_MIN_CSS).toPath();
-            //concatenate all sortedFiles into all.min.css
-            concatenateCss(new File(destDir, FOLDER_CSS).toPath(), allMinCssPath);
+            lessCompiler.compile(new File(tempDir, PROJECT_LESS), new File(tempDir, PROJECT_CSS));
+            lessCompiler.compile(new File(tempDir, BOOTSTRAP_LESS), new File(tempDir, BOOTSTRAP_CSS));
+            lessCompiler.compile(new File(tempDir, LOADER_LESS), new File(tempDir, LOADER_CSS));
 
-            //reload content for css controller
-            context.setAttribute(customerName, null);
+            Path allMinCssPath = new File(tempDir, ALL_MIN_CSS).toPath();
+            //concatenate all sortedFiles into all.min.css
+            String css = concatenateCss(context, new File(tempDir, FOLDER_CSS).toPath(), allMinCssPath);
+
+            //set content for css controller
+            context.setAttribute(customerName, css);
         }
     }
 
-    private void concatenateCss(Path path, Path outFile) throws FileNotFoundException, IOException {
+    private String concatenateCss(ServletContext context, Path path, Path outFile) throws FileNotFoundException, IOException {
         DirectoryStream<Path> cssFiles = Files.newDirectoryStream(path, "*.css");
-        if (Files.exists(outFile)){
+        if (Files.exists(outFile)) {
             Files.delete(outFile);
         }
         List<Path> sortedFiles = new ArrayList<>();
         for (Path cssFile : cssFiles) {
             sortedFiles.add(cssFile);
         }
-        
+
         Collections.sort(sortedFiles, new PathByFileNameComparator());
-        
+
         for (Path cssFile : sortedFiles) {
             Files.write(outFile, Files.readAllBytes(cssFile), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         }
-    }
-    
-    public String getAllMinCss(ServletContext context, String customerName) throws IOException{
-        String cssContent = (String) context.getAttribute(customerName);
-        if (cssContent == null){
-            cssContent = reloadAllMinCss(context, customerUtil.getCustomerDir(customerName));
-            context.setAttribute(customerName, cssContent);
-        }
-        return cssContent;
-    }
 
-    private String reloadAllMinCss(ServletContext context, File customerDir) throws IOException {
         byte[] cssData;
-        Path cssFile = new File(customerDir, ALL_MIN_CSS).toPath();
-        if (Files.exists(cssFile)){
-            cssData = Files.readAllBytes(cssFile);
+        if (Files.exists(outFile)) {
+            cssData = Files.readAllBytes(outFile);
         } else {
             //read from classpath
             InputStream is = context.getResourceAsStream(ALL_MIN_CSS);
@@ -201,10 +177,14 @@ public class HtmlResourceUtil {
         return css;
     }
 
+    public String getAllMinCss(ServletContext context, String customerName) throws IOException {
+        return (String) context.getAttribute(customerName);
+    }
+
     private void copyResources(ServletContext context, String sourceFolder, File destinationFolder) throws MalformedURLException, IOException {
         Set<String> resourcePaths = context.getResourcePaths(sourceFolder);
-        for (String resourcePath: resourcePaths){
-            if (resourcePath.endsWith("/")){ //must be a directory
+        for (String resourcePath : resourcePaths) {
+            if (resourcePath.endsWith("/")) { //must be a directory
                 copyResources(context, resourcePath, destinationFolder);
             } else {
                 URL resource = context.getResource(resourcePath);
@@ -218,37 +198,19 @@ public class HtmlResourceUtil {
         Path outputPath = new File(destDir, FILE_NAME).toPath();
         String content = IOUtils.toString(lessIs, Constants.UTF8);
         for (CssAttribute attribute : cssAttributes) {
-            if (!StringUtils.isEmpty(attribute.getCssValue())){
+            if (!StringUtils.isEmpty(attribute.getCssValue())) {
                 content = content.replaceAll(attribute.getCssDefaultValue(), attribute.getCssValue());
             }
         }
 
         //overwrite variables.less in data directory
-        if (!Files.exists(outputPath)){
-            if (!Files.exists(outputPath.getParent())){
+        if (!Files.exists(outputPath)) {
+            if (!Files.exists(outputPath.getParent())) {
                 Files.createDirectory(outputPath.getParent());
             }
             Files.createFile(outputPath);
         }
         Files.write(outputPath, content.getBytes(Constants.UTF8), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-    }
-    
-    public String getCssFile(ServletContext context, String name, String customer) throws IOException{
-        File customerDir = customerUtil.getCustomerDir(customer);
-        File cssDir = new File(customerDir, "css");
-        File file = new File(cssDir, name);
-        try {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            return new String(bytes, Constants.UTF8);
-        } catch (IOException e){
-            InputStream stream = context.getResourceAsStream("/static/css/"+name);
-            if (stream == null){
-                LOG.warn("Unable to load fallback /static/css/"+name);
-                return null;
-            }
-            return IOUtils.toString(stream, Constants.UTF8);
-        }
-        
     }
 
     public List<CssAttribute> getDefaultCssAttributes() {
@@ -278,19 +240,19 @@ public class HtmlResourceUtil {
     public List<CssAttribute> getCssAttributes() {
         List<CssAttribute> cssAttributes = cssAttributeDAO.findAll();
         List<CssAttribute> defaultCssAttributes = getDefaultCssAttributes();
-        for (CssAttribute defaultAttribute: defaultCssAttributes){
+        for (CssAttribute defaultAttribute : defaultCssAttributes) {
             boolean exists = false;
-            for (CssAttribute att: cssAttributes){
-                if (att.getName().equals(defaultAttribute.getName())){
+            for (CssAttribute att : cssAttributes) {
+                if (att.getName().equals(defaultAttribute.getName())) {
                     exists = true;
                     break;
                 }
             }
-            if (!exists){
+            if (!exists) {
                 cssAttributes.add(defaultAttribute);
             }
         }
-        
+
         Collections.sort(cssAttributes, new CssAttributeComparator());
         return cssAttributes;
     }
