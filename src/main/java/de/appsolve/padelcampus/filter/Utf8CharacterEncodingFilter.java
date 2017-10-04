7dc44d866cf97b9466352bc800cade51823f4ed5
Dominik <d.wisskirchen@gmail.com>
2017-10-04 22:18:03 +0200

    add additional exceptions to character encoding filter

--- src/main/java/de/appsolve/padelcampus/filter/Utf8CharacterEncodingFilter.java
+++ src/main/java/de/appsolve/padelcampus/filter/Utf8CharacterEncodingFilter.java
@@ -9,16 +9,16 @@ import java.nio.charset.StandardCharsets;
 
 public class Utf8CharacterEncodingFilter extends CharacterEncodingFilter {
 
-    public Utf8CharacterEncodingFilter() {
-        super(StandardCharsets.UTF_8.name(), true, true);
-    }
+  public Utf8CharacterEncodingFilter() {
+    super(StandardCharsets.UTF_8.name(), true, true);
+  }
 
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String url = request.getRequestURI();
-        if (!StringUtils.isEmpty(url) && (url.startsWith("/images/image") || url.startsWith("/static/css/fonts"))) {
-            return true;
-        }
-        return super.shouldNotFilter(request);
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    String url = request.getRequestURI();
+    if (!StringUtils.isEmpty(url) && (url.startsWith("/images/image") || url.startsWith("/pro/images") || url.startsWith("/pro/video") || url.startsWith("/static/css/fonts"))) {
+      return true;
     }
+    return super.shouldNotFilter(request);
+  }
 }
