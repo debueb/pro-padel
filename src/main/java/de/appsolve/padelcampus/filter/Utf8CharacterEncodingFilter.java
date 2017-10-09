54a7089ad2c7fb397bf7db57cd2e69c1850cc03d
Dominik <d.wisskirchen@gmail.com>
2017-10-09 10:18:02 +0200

    add fonts to utf8 character encoding filter exception list

--- src/main/java/de/appsolve/padelcampus/filter/Utf8CharacterEncodingFilter.java
+++ src/main/java/de/appsolve/padelcampus/filter/Utf8CharacterEncodingFilter.java
@@ -6,8 +6,24 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
 
 public class Utf8CharacterEncodingFilter extends CharacterEncodingFilter {
+
+  private static Pattern[] NO_UTF8_HEADER_PATTERNS = new Pattern[]{
+    Pattern.compile("^/images/image.*$"),
+    Pattern.compile("^/pro/images.*$"),
+    Pattern.compile("^/pro/video.*$"),
+    Pattern.compile(".*\\.eot"),
+    Pattern.compile(".*\\.svg"),
+    Pattern.compile(".*\\.ttf"),
+    Pattern.compile(".*\\.woff"),
+    Pattern.compile(".*\\.png"),
+    Pattern.compile(".*\\.jpg"),
+    Pattern.compile(".*\\.jpeg"),
+    Pattern.compile(".*\\.gif"),
+  };
+
 
   public Utf8CharacterEncodingFilter() {
     super(StandardCharsets.UTF_8.name(), true, true);
@@ -16,8 +32,12 @@ public class Utf8CharacterEncodingFilter extends CharacterEncodingFilter {
   @Override
   protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
     String url = request.getRequestURI();
-    if (!StringUtils.isEmpty(url) && (url.startsWith("/images/image") || url.startsWith("/pro/images") || url.startsWith("/pro/video") || url.startsWith("/static/css/fonts"))) {
-      return true;
+    if (!StringUtils.isEmpty(url)) {
+      for (Pattern pattern : NO_UTF8_HEADER_PATTERNS) {
+        if (pattern.matcher(url).matches()) {
+          return true;
+        }
+      }
     }
     return super.shouldNotFilter(request);
   }
