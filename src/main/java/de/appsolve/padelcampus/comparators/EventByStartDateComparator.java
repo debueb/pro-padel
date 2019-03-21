6c932d36eb5b94b849099f6a6c9edf1472388bb5
Dominik <d.wisskirchen@gmail.com>
2019-03-21 09:06:17 +0100

    sort events alphanumerically

--- src/main/java/de/appsolve/padelcampus/comparators/EventByStartDateComparator.java
+++ src/main/java/de/appsolve/padelcampus/comparators/EventByStartDateComparator.java
@@ -6,6 +6,7 @@
 package de.appsolve.padelcampus.comparators;
 
 import de.appsolve.padelcampus.db.model.Event;
+import se.sawano.java.text.AlphanumericComparator;
 
 import java.io.Serializable;
 import java.util.Comparator;
@@ -19,6 +20,8 @@ public class EventByStartDateComparator implements Comparator<Event>, Serializab
 
     private Boolean reverse = false;
 
+    private AlphanumericComparator alphanumericComparator = new AlphanumericComparator();
+
     public EventByStartDateComparator() {
     }
 
@@ -30,7 +33,7 @@ public class EventByStartDateComparator implements Comparator<Event>, Serializab
     public int compare(Event o1, Event o2) {
         int result = o1.getStartDate().compareTo(o2.getStartDate());
         if (result == 0) {
-            result = o1.getName().compareToIgnoreCase(o2.getName());
+            return alphanumericComparator.compare(o1.getName(), o2.getName());
         }
         return reverse ? result * -1 : result;
     }
